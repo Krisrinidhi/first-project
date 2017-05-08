a0f1b8f9cd808e3d96e305f6e69641516134e65c
Krisrinidhi <sk2423@njit.edu>
2017-05-08 15:45:54 -0400

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,13 +1,6 @@
 <?php
-
-/* Database Connection */
-
-$sDbHost = 'sql2.njit.edu';
-$sDbName = 'sk2423';
-$sDbUser = 'sk2423';
-$sDbPwd = 'IR8VDFjJC';
-
-$dbConn = mysql_connect ($sDbHost, $sDbUser, $sDbPwd) or die ('MySQL connect failed. ' . mysql_error());
-mysql_select_db($sDbName,$dbConn) or die('Cannot select database. ' . mysql_error());
+session_start();
+//connect to database
+$db = mysqli_connect("sql2.njit.edu","sk2423","IR8VDFjJC","sk2423");
 
 ?>
