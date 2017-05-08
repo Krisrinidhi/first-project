7a3e1ba9fb78bf7970e05409e8391ad271da34cb
Krisrinidhi <sk2423@njit.edu>
2017-05-08 16:55:55 -0400

    Update register.php

--- register.php
+++ register.php
@@ -1,5 +1,7 @@
 <?php
-include( ./'model'/'database.php');
+session_start();
+//connect to database
+$db = mysqli_connect("sql2.njit.edu","sk2423","IR8VDFjJC","sk2423");
 
 if(isset($_POST['register_btn']))
 {
