8f494bc4f78b65a705a4b21d99fa9afa142bd1ab
Krisrinidhi <sk2423@njit.edu>
2017-05-08 15:51:12 -0400

    Update insert_complete.php

--- controller/insert_complete.php
+++ controller/insert_complete.php
@@ -61,9 +61,7 @@ echo '<div style="padding:4px; border:1px solid red; color:red;">'.$error.'</div
 <?php
 }
 
-session_start(); 
-//connect to database
-$db = mysqli_connect("sql2.njit.edu","sk2423","IR8VDFjJC","sk2423");
+include( ./model/database.php);
 
 if (isset($_POST['submit']))
 {
