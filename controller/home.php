726beb93965d9b791c83525f9d416c4d84ede8a6
Krisrinidhi <sk2423@njit.edu>
2017-05-08 15:47:33 -0400

    home file updated

--- controller/home.php
+++ controller/home.php
@@ -4,37 +4,39 @@
 <body>
 <head>
   <title>HOME</title>
-  <link rel="stylesheet" type="text/css" href="style.css"/>
+  <link rel="stylesheet" type="text/css" href=" ../style.css"/>
 </head>
 <div class="header">
     <h1>HOME PAGE</h1>
 </div>
-<h1>WELCOME TO YOUR PAGE</h1>
+<h1><center>WELCOME TO YOUR PAGE</center></h1>
 <div>
-    <h4>TABLES<?php echo $_SESSION['firstname']; 
-?></h4></div>
+    <h4><center>TABLES<?php echo $_SESSION['firstname']; 
+?></center></h4></div>
 </div>
-<?php
-session_start(); 
-//connect to database
-$db = mysqli_connect("sql2.njit.edu","sk2423","IR8VDFjJC","sk2423");
-$email=mysqli_real_escape_string($db,$_POST['email']);
+
+
+<p><h3><center>INCOMPLETE RECORDS</center></h3></p>
+<center><?php
+
+include( ./model/database.php);
 $Incomplete_query = "SELECT Id, Email, ToDoItem, Description , Status, DueDate , DueTime FROM todo WHERE  Status= 'incomplete'  ";
 // AND Email='krisrinidhi@gmail.com' ";
 //Email= '$Email' and
 $result_count_1 = mysqli_query($db,$Incomplete_query);
  if (mysqli_num_rows($result_count_1)>0)
  {
-		
+
 			echo "<table border='1' cellpadding='10'>";
+    
 			echo "<tr>
-			<th><font color='Black'>ToDoItem</font></th>
-			<th><font color='Black'>Description</font></th>
-			<th><font color='Black'>DueDate</font></th>
-			<th><font color='Black'>DueTime</font></th>
-			<th><font color='Black'>Edit</font></th>
-			<th><font color='Black'>Delete</font></th>
-			<th><font color='Black'>check off</font></th>
+			<center><th><font color='Red'>ToDoItem</font></th>
+			<th><font color='Red'>Description</font></th>
+			<th><font color='Red'>DueDate</font></th>
+			<th><font color='Red'>DueTime</font></th>
+			<th><font color='Red'>Edit</font></th>
+			<th><font color='Red'>Delete</font></th>
+			<th><font color='Red'>check off</font></th></center>
 			</tr>";
 	  while($row = mysqli_fetch_assoc($result_count_1)) 
 	  {
@@ -50,14 +52,18 @@ $result_count_1 = mysqli_query($db,$Incomplete_query);
 			echo "</tr>";
 	  }
 	  echo "</table>";
+  
+  
 }
-?>
-<p><a href="insert.php">Insert new incomplete record</a></p>
-<?php
+?> </center>
+<p><center><a href="insert.php">Insert new incomplete record</a><center></p>
+<br></br>
+<p><h3><center>COMPLETE RECORDS</center></h3></p>
+<center><?php
+
 session_start(); 
 //connect to database
 $db = mysqli_connect("sql2.njit.edu","sk2423","IR8VDFjJC","sk2423");
-$email=mysqli_real_escape_string($db,$_POST['email']);
 
 
 
@@ -99,8 +105,8 @@ $result_count = mysqli_query($db,$complete_query);
  }
  
 
- ?>
-<p><a href="insert_complete.php">Insert new complete record</a></p>
-<a href="logout.php">Log Out</a>
+ ?></center>
+<p><center><a href="insert_complete.php">Insert new complete record</a></center></p>
+<a href="logout.php"><center>Log Out</center></a>
 </body>
 </html>
