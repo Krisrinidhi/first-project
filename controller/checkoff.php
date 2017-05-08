20361f9c317dc714d7e3e029e635f225bc7ce8f8
Krisrinidhi <sk2423@njit.edu>
2017-05-08 16:50:41 -0400

    Update checkoff.php

--- controller/checkoff.php
+++ controller/checkoff.php
@@ -1,5 +1,7 @@
 <?php
-include( ./model/databse.php);
+session_start();
+//connect to database
+$db = mysqli_connect("sql2.njit.edu","sk2423","IR8VDFjJC","sk2423");
 
 if (isset($_GET['Id']) && is_numeric($_GET['Id']))
 { 		
